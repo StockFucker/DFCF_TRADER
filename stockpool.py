fff7ccd8c23eb8e8aff7f384cbe64b002a664719
Arnold1974 <wangych_qd@163.com>
2017-05-26 02:46:24 +0800

    2017/05/25

--- stockpool.py
+++ stockpool.py
@@ -19,18 +19,16 @@ class StockPool(object):
     def generatge_basic_stockcode(self):
         df = ts.get_stock_basics()
         self.stockIDs=df.index
-        
-        df = ts.get_today_all()
-        df[df['name'].str.contains('[ST]').values==False] #去掉股票代码包含ST的股票
-        #df[(df['name'].str.contains('[ST]').values==False)&(df['trade']==0)] 
-        
-    def generate_stock(self):
-        pass
+                
+    def generate_stockpool(self):
+        info =  ts.get_today_all() # ts.get_stock_basics()
+        self.stock_pool = info[(info['name'].str.contains('[ST]').values==False) & (info['open'] <> 0)]  #去除掉 ST & 未交易
         
     def get_hist_data(self,code,start,end):
         df = ts.get_hist_data(code=code,start=start,end=end,ktype='D',retry_count=3,pause=0.001)
         return df
 
+    #判断是否窗新高
     def is_break_high(self,stockID,days):  
         #end_day=datetime.date(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day)  
         #end_day=end_day.strftime("%Y/%m/%d")  
@@ -38,7 +36,7 @@ class StockPool(object):
         #start_day=self.calendar.trade_calendar(end_day.replace('-','/'),days).replace('/','-')
         df=ts.get_hist_data(stockID,start=start_day,end=end_day)  
         if df is None: return False
-        if len(df)==0 or df.empty or df.index[0] <> end_day:
+        if len(df)==0 or df.index[0] <> end_day:
             return False
         period_high=df['high'].max()  #区间最高价
         period_low=df['low'].min()
@@ -54,30 +52,62 @@ class StockPool(object):
             return True  
         else:  
             return False  
-        
+    
+    #判断是否涨停    
+    def is_top_price(self,stockID,before=1):
+        df=ts.get_k_data(stockID)[::-1]
+        if len(df)>15 and df.iloc[0].date == end_day:
+            if round(df.iloc[before-1].close,2) == round(round(df.iloc[before].close,2)*1.1, 2):
+                buy_value = self.get_buy_value(df,before)
+                return True,buy_value
+            else:
+                return False
+        else:
+            return False
+    
+    #获取均线数值    
+    def get_ma(self,stockID,ma=5):
+        sum=0
+        df=ts.get_k_data(stockID)[::-1]
+        if df.empty:
+            return 0
+        elif len(df)<ma:
+            ma=len(df)
+        for _ in xrange(ma):
+            sum+=round(df.iloc[_].close,2)
+        return round(sum/ma,2)
+    
+    #获取买入价格 涨停价格后两天内距离最高点 下跌 7%
+    def get_buy_value(self,df,before,fall=7):
+        print df
+        print round(df.iloc[0:before]['high'].max()*(100-fall)/100.0, 2)
+        return round(df.iloc[0:before]['high'].max()*(100-fall)/100.0, 2)
+
 if __name__=="__main__":
     test=StockPool()
-    stockID ,start,end='600000', '2017-05-10', '2017-05-18'
-    df= test.get_hist_data(stockID,start,end)
-    print df
-    a= df.ix[0,['ma5']] #df.ix['2017-05-17',['ma5']]
-    print a
-    print type(a)
-    print a[0]
-    
-    info=ts.get_stock_basics()  
+    test.generate_stockpool()
+    info=test.stock_pool
+
     today=time.strftime('%Y-%m-%d',time.localtime())
     end_day=test.calendar.trade_calendar(today.replace('-','/'),-2).replace('/','-')
     start_day=test.calendar.trade_calendar(end_day.replace('-','/'),-10).replace('/','-')
+    
     i=1;stocks={}
     print len(info)
+    
     def loop_all_stocks():
         global i
-        for EachStockID in info.index:  
+        for EachStockID in info.code: # info.index:  
              print i,EachStockID;i+=1
-             if test.is_break_high(EachStockID,-10):  
-                 print "High price on",  
+             #if test.is_break_high(EachStockID,-10):
+             result= test.is_top_price(EachStockID,2)
+             if result:
+                 print "High price on",
                  print EachStockID,  
-                 print info.ix[EachStockID]['name'].decode('utf-8')  
-                 stocks[EachStockID]=info.ix[EachStockID]['name'].decode('utf-8')
+                 print info[info.code==EachStockID].name.values[0]  #info.ix[EachStockID]['name'].decode('utf-8')  
+                 stocks[EachStockID] = info[info.code==EachStockID].name.values[0],test.get_ma(EachStockID),result[1] #info.ix[EachStockID]['name'].decode('utf-8')
     loop_all_stocks() 
+    
+    for _ in stocks:
+        print _,stocks[_]
+        
